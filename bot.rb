588f7ccfedbb7f22548c9146e9e362e27103b005
will <33510837+danrfq@users.noreply.github.com>
2022-04-23 22:57:22 +0700

    redact token

--- bot.rb
+++ bot.rb
@@ -1,7 +1,7 @@
 require "discordrb"
 require_relative "jishacum/jishacum" # import the jishacum container file
 
-bot = Discordrb::Commands::CommandBot.new token: "OTAyOTkzMTY2MjgwNTcyOTM4.YXmf6w.jkX8__1cs9HSuryBppJXLWRvim8", prefix: "!", ignore_bots: true
+bot = Discordrb::Commands::CommandBot.new token: "YOUR_BOT_TOKEN", prefix: "!", ignore_bots: true
 
 bot.include! Jishacum # include the jishacum container to your bot
 

be8ce134c6766a3a846f8a3ff1a276a6e4e7a48a
will <33510837+danrfq@users.noreply.github.com>
2022-05-04 19:39:40 +0700

    for swarley testing

--- jishacum/config.rb
+++ jishacum/config.rb
@@ -1,6 +1,6 @@
 module JishacumConfig
     JISHACUM_CONFIG = {
-        "OWNER_ID" => 211756205721255947,
+        "OWNER_ID" => 171764626755813376,
         "OWNER_IDS" => [],
         "MAIN_BOT_FILE_NAME" => "bot.rb",
         "IGNORE_DMS" => true,
